ee6d6a74453136a5c6134dd083011c818e919766
daniloui001 <daniel.louie@stonybrook.edu>
2023-09-19 21:39:52 +0000

    added some code

--- analysis.py
+++ analysis.py
@@ -4,10 +4,10 @@ import numpy as np
 from scipy.stats import skew, kurtosis, shapiro
 import faker as Faker
 
-df = pd.read_csv('https://raw.githubusercontent.com/daniloui001/datasci_3_eda/main/processed/IHME.CSV')
+df = pd.read_csv('https://raw.githubusercontent.com/daniloui001/datasci_3_eda/main/processed/us_census.csv')
 ###print(df.columns)
 
-print(df['country'])
+print(df['AGE'])
 print(df.dtypes)
 
-print(df.country.describe())
+print(df.AGE.describe())
