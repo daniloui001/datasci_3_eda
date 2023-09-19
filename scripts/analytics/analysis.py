a69b9ac61a9587b1192d1238d26de3729d758415
daniloui001 <daniel.louie@stonybrook.edu>
2023-09-19 21:25:57 +0000

    add

--- scripts/analytics/analysis.py
+++ scripts/analytics/analysis.py
@@ -4,5 +4,5 @@ import numpy as np
 from scipy.stats import skew, kurtosis, shapiro
 import faker as Faker
 
-df = pd.read_csv('https://raw.githubusercontent.com/daniloui001/datasci_3_eda/main/processed/IHME_PREM_CH_HEALTH_2021_Y2021M09D14.CSV.CSV')
+df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA_507_2023/main/WK3/data/trinetx/lab_result.csv')
 df.columns
